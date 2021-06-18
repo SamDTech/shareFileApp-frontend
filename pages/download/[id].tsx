d8e5f4c84d4b81667cc6794d2e2fbf26bfcb3f40
samdtech <opeyemisamuel222@gmail.com>
2021-06-18 14:13:05 +0100

    ready for deployment

--- pages/download/[id].tsx
+++ pages/download/[id].tsx
@@ -9,12 +9,9 @@ const Download: NextPage<{ file: IFile }> = ({
   file: { format, name, sizeInBytes, id },
 }) => {
   const handleDownload = async () => {
-    const { data } = await axios.get(
-      `http://localhost:8000/api/files/${id}/download`,
-      {
-        responseType: "blob",
-      }
-    );
+    const { data } = await axios.get(`/api/files/${id}/download`, {
+      responseType: "blob",
+    });
 
     fileDownload(data, name);
   };
@@ -62,7 +59,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   try {
     const { data } = await axios.get<IFile>(
-      `http://localhost:8000/api/files/${id}`
+      `${process.env.API_BASE_ENDPOINT}/api/files/${id}`
     );
 
     file = data;
